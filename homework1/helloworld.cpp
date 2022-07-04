58868baa1165bc810d5fac801b3db3587ceae9a0
Ivan Grafskiy <igrafskiy@gmail.com>
2022-07-04 06:35:01 +0300

    What ToDo

--- homework1/helloworld.cpp
+++ homework1/helloworld.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+// ToDo:
+// 1. add version
+// 2. github actions
+// 3. lint checker
+
 int main(){
   std::cout << "build N" << std::endl;
   std::cout << "Hello, World!" << std::endl;
