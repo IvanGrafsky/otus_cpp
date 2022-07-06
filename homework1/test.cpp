3613fd8f3a0f7383b2c026b7dec8173e77d3b6f6
Ivan Grafskiy <igrafskiy@gmail.com>
2022-07-06 06:59:02 +0300

    Alive test

--- homework1/test.cpp
+++ homework1/test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main(){
+#include <gtest/gtest.h>
 
-  return 0;
+TEST(VersionTest, CheckVersion) {
+  ASSERT_EQ(0, 0);
+}
+
+int main(int argc, char **argv){
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
