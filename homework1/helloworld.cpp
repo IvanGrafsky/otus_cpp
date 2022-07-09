db723bff1001b0cebf196edd12ad6b8195ade753
Ivan Grafskiy <igrafskiy@gmail.com>
2022-07-09 13:02:19 +0300

    Removed comments

--- homework1/helloworld.cpp
+++ homework1/helloworld.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 #include "lib.h"
 
-// ToDo:
-// 2. github actions
-// 4. deb packet
-// 3. lint checker
-
 int main(){
   std::cout << "build " << version() << std::endl;
   std::cout << "Hello, World!" << std::endl;
