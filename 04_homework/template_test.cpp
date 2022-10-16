8abf873c34d0e1decdf6b855b7cf7ef11ab8264c
Ivan Grafskiy <igrafskiy@gmail.com>
2022-10-16 20:03:22 +0300

    Added CMake

--- 04_homework/template_test.cpp
+++ 04_homework/template_test.cpp
@@ -1,45 +1,4 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <list>
-#include <tuple>
-#include <type_traits>
-
-template<typename T, std::enable_if_t<std::is_arithmetic<T>::value, bool> = true>
-void print_ip(T t){
-  std::string result;
-  unsigned char* u = reinterpret_cast<unsigned char*>(&t);
-  for(size_t i = sizeof(T); i > 0; i--){
-    result += std::to_string(*(u + i - 1)) + ((i == 1) ? "" : ".");
-  }
-  std::cout << result << std::endl;
-}
-
-template<typename U, std::enable_if_t<std::is_same<char const*, decltype(std::declval<U>().c_str())>::value, bool> = true>
-void print_ip(const U& container){
-  std::cout << container.c_str() << std::endl;
-}
-
-template<typename U, std::enable_if_t<!std::is_same<char, typename U::value_type>::value, bool> = true>
-void print_ip(const U& container) {
-  for(auto t = container.cbegin(); t != container.cend(); t = next(t)) {
-    std::cout << *t << ((next(t) != container.cend()) ? "." : "\n");
-  }
-}
-
-template<typename ...Ts, std::enable_if_t<
-                std::is_same<typename std::tuple_size<std::tuple<Ts...>>::value_type, std::size_t>::value, bool> = true>
-void print_ip(const std::tuple<Ts...> tup){
-  std::apply
-    (
-        [](Ts const&... tupleArgs)
-        {
-            std::size_t n{0};
-            ((std::cout << tupleArgs << (++n != sizeof...(Ts) ? "." : "\n")), ...);
-        }, tup
-    );
-}
-
+#include <print_lib.hpp>
 
 int main(){
   print_ip( int8_t{-1} ); // 255
